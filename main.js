57cb846bbccf985dba9a6268e2eb278512efe0e8
Sergio Ferreira <sergiof.ferreira8@gmail.com>
2024-01-28 13:35:48 -0300

    fix: limitaçãi de casa decimais

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ form.addEventListener('submit', function(e){
     function atualizaMediaFinal(){
       const mediaFinal = calculaMediaFinal()
 
-      document.getElementById('media-final-valor').innerHTML = mediaFinal
+      document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
       document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado
 
        
